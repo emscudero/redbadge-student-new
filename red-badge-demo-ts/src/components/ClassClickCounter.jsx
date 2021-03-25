d91f7838f9f40efbc85878f264b48251749f1ac8
Emily Scudero <emily@MSI.COM>
2021-03-25 19:35:43 -0400

    first push

--- red-badge-demo-ts/src/components/ClassClickCounter.jsx
+++ red-badge-demo-ts/src/components/ClassClickCounter.jsx
@@ -1,1 +1,22 @@
 // Create a class version of the Functional Component
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+import {Button, Container} from "reactstrap";
+
+class ClassClickCounter extends Component {
+    constructor(props)
+    super(props)
+    this.state = { }
+}
+
+handleClick = () => {
+    this.setState({})
+}
+
+render(){
+    return(
+        <Container>
+            <p>The total clicks left are {this.state.countValue}</p>
+        </Container>
+    )
+}
