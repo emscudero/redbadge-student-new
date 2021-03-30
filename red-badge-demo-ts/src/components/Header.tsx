33c2dd725457ded41ab546efc2a7b459d5194285
Emily Scudero <emily@MSI.COM>
2021-03-30 17:41:53 -0400

    saturday class

---
+++ red-badge-demo-ts/src/components/Header.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+export interface HeaderProps {}
+ 
+const Header: React.SFC<HeaderProps> = () => {
+    return ( <h1>Click to Roll</h1> );
+}
+ 
+export default Header;
