594952da4f248fe636bb0b5fcd990bc41166693d
Emily Scudero <emily@MSI.COM>
2021-04-20 18:05:58 -0400

    basic html update

--- typescript-day-01/01 - basic-html/basic.js
+++ typescript-day-01/01 - basic-html/basic.js
@@ -8,3 +8,18 @@
 // the function addNumbers should return num1+num2
 
 // inside function handleClick call the addNumbers function and console log the result comingback from addNumbers
+
+let button = document.getElementById("btn");
+
+let input1 = document.getElementById("number1"); // or parseFloat to add together
+let input2 = document.getElementById("number2");
+
+button.addEventListener("click", () => {
+  console.log(addNumbers(input1.nodeValue, input2.value));
+});
+
+function addNumbers(num1, num2) {
+  console.log(typeof num1);
+
+  return num1 + +num2; // or Number (num1) + Number(num2) to add it versus concade it
+}
